c7ca55e34c85ae51bd864af5e0a5521cade3db8b
Guillaume Vincent <gvincent@redhat.com>
2022-06-15 10:09:30 +0000

    wip

--- beaker_server/entrypoint.sh
+++ beaker_server/entrypoint.sh
@@ -6,7 +6,7 @@ function log_info {
 
 # Poll until MySQL responds to our ping.
 function wait_for_mysql() {
-  while ! mysqladmin -h 127.0.0.1 -u root -ppassword ping &>/dev/null; do
+  while ! mysqladmin -h db -u beaker -pbeaker ping &>/dev/null; do
     log_info "Waiting for MySQL to start ..."
     sleep 1
   done
@@ -15,15 +15,14 @@ function wait_for_mysql() {
 
 # Initiate the beaker database
 function init_beaker_db() {
-  beaker-init -u admin -p testing -e root@localhost.localdomain 
-  touch /etc/beaker/already_initiate
+  beaker-init -u admin -p admin -e root@localhost 
+  touch /tmp/already_initiate
 }
 
 wait_for_mysql
 
-if [ ! -f "/etc/beaker/already_initiate" ]; then
+if [ ! -f "/tmp/already_initiate" ]; then
   init_beaker_db
 fi
 
-su -s /bin/bash apache -c "/usr/bin/beakerd -p /run/beaker/beakerd.pid -f" &
-sleep infinity
+exec "$@"
