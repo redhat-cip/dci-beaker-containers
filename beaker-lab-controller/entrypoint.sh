6c3c1dddab1fde6fc2591075dc180d40c8f3c410
Guillaume Vincent <gvincent@redhat.com>
2022-07-07 01:26:26 +0000

    Fix beaker-lab-controller
    Beaker lab controller container can consume and use beaker-server domain name.

---
+++ beaker-lab-controller/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function log_info {
+  echo "---> `date +%T`     $@"
+}
+
+function wait_for_http_ok() {
+  while [[ `curl -IL ${0} -o /dev/null -w '%{http_code}' -s` -eq 200 ]]; do
+    log_info "Waiting for Beaker server to start..."
+    sleep 1
+  done
+  log_info "Beaker server started successfully" && return 0
+}
+
+# Poll until Beaker server responds to our ping.
+wait_for_http_ok "http://beaker-server/bkr/"
+
+exec "$@"
